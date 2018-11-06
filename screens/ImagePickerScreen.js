af813794d5a1d2d11492c7920f4a083a58e5d203
KarinaDavtyan <k.davtyan27@gmail.com>
2018-11-06 19:19:43 +0100

    style(ImagePickerScreen): improve layoute of the buttons, add takePhoto method

--- screens/ImagePickerScreen.js
+++ screens/ImagePickerScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { Text, View, StatusBar, Image } from 'react-native';
 import { Icon, Button } from 'react-native-elements'
 import { Camera, Permissions } from 'expo';
 import { withNavigation } from 'react-navigation';
@@ -14,13 +14,29 @@ class RoutesScreen extends React.Component {
 
   state = {
     hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
+    image: ''
   };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
   }
+
+  takePicture = async () => {
+    console.log('here');
+    if (this.camera) {
+      await this.camera.takePictureAsync()
+        .then(data => {
+          this.setState((state) => {
+            return {
+              ...state,
+              image: data.uri
+            }
+          })
+        })
+    }
+    console.log(this.state);
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -32,32 +48,62 @@ class RoutesScreen extends React.Component {
       return (
         <View style={{ flex: 1 }}>
           <StatusBar hidden />
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+
+          <Camera
+            style={{ flex: 1,  justifyContent: 'space-between'}}
+            type={this.state.type}
+            ref={ref => { this.camera = ref }}
+          >
+            <Button
+              onPress={() => this.props.navigation.goBack()}
+              buttonStyle={{
+                backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                width: 60,
+                height: 60,
+                borderColor: 'transparent',
+                borderWidth: 0,
+                borderRadius: 50,
+              }}
+              title=''
+              icon={
+                <Icon
+                  name='close'
+                  size={30}
+                  color='black'
+                />}
+              containerStyle={{ marginTop: 20, marginLeft: 20 }}
+            />
             <View
               style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
+                flex: 0,
+                backgroundColor: 'rgba(255, 255, 255, 0)',
+                height: 120,
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
+              <Image
+                source={require('../assets/images/icon-photo-gallery-48-white.png')}
+                style={{
+                  height: 40,
+                  width: 40,
+                  position: 'absolute',
+                  left: 10,
+                  bottom: 10
+                }}
+              />
               <Button
-                onPress={() => this.props.navigation.goBack()}
+                onPress={() => this.takePicture()}
                 buttonStyle={{
-                  backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                  backgroundColor: 'rgba(255, 255, 255, 0)',
                   width: 60,
                   height: 60,
-                  borderColor: 'transparent',
-                  borderWidth: 0,
+                  borderColor: 'rgba(255, 255, 255, 1)',
+                  borderWidth: 6,
                   borderRadius: 50,
                 }}
                 title=''
-                icon={
-                  <Icon
-                    name='close'
-                    size={30}
-                    color='black'
-                  />}
-                containerStyle={{ marginTop: 20, marginLeft: 20 }}
-              />
+              >
+              </Button>
             </View>
           </Camera>
         </View>
