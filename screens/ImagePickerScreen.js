b687a84702dbc684edc3e56543d832047b2a1880
KarinaDavtyan <k.davtyan27@gmail.com>
2018-11-08 15:24:57 +0100

    feat(imagePickerScreen): get photo from phone's camera roll

--- screens/ImagePickerScreen.js
+++ screens/ImagePickerScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, StatusBar, Image } from 'react-native';
+import { Text, View, StatusBar, Image, TouchableOpacity } from 'react-native';
 import { Icon, Button } from 'react-native-elements'
-import { Camera, Permissions } from 'expo';
+import { Camera, Permissions, ImagePicker } from 'expo';
 import { withNavigation } from 'react-navigation';
 
 
-class RoutesScreen extends React.Component {
+class ImagePickerScreen extends React.Component {
 
     //this is the top bar
     static navigationOptions = {
@@ -14,31 +14,56 @@ class RoutesScreen extends React.Component {
 
   state = {
     hasCameraPermission: null,
+    hasCameraRollPermission: false,
     image: ''
   };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+    this.setState((state) => {
+      return {
+        ...state,
+        hasCameraPermission: status === 'granted'
+      }
+    });
+
+    const permissionsCameraRoll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    this.setState((state) => {
+      return {
+        ...state,
+        hasCameraRollPermission: permissionsCameraRoll.status === 'granted'
+      }
+    });
   }
 
   takePicture = async () => {
-    console.log('here');
+    //eslint-disable-next-line
     if (this.camera) {
       await this.camera.takePictureAsync()
         .then(data => {
-          // this.setState((state) => {
-          //   return {
-          //     ...state,
-          //     image: data.uri
-          //   }
-          // })
           if (data.uri) {
             this.props.navigation.navigate('DrawingScreen', {
               imageUri: data.uri
             });
           }
         })
+    }
+  };
+
+  pickImage = async () => {
+    const { hasCameraRollPermission } = this.state;
+    if (hasCameraRollPermission) {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaType: 'Image'
+      });
+      if (!result.cancelled) {
+        if (result.uri) {
+          this.props.navigation.navigate('DrawingScreen', {
+            imageUri: result.uri
+          });
+        }
+        this.setState({ image: result.uri });
+      }
     }
   };
 
@@ -59,7 +84,7 @@ class RoutesScreen extends React.Component {
             ref={ref => { this.camera = ref }}
           >
             <Button
-              onPress={() => this.props.navigation.goBack()}
+              onPress={() => this.props.navigation.navigate('Routes')}
               buttonStyle={{
                 backgroundColor: 'rgba(255, 255, 255, 0.6)',
                 width: 60,
@@ -85,16 +110,22 @@ class RoutesScreen extends React.Component {
                 justifyContent: 'center',
                 alignItems: 'center',
               }}>
-              <Image
-                source={require('../assets/images/icon-photo-gallery-48-white.png')}
+              <TouchableOpacity
+                onPress={() => this.pickImage()}
                 style={{
-                  height: 40,
-                  width: 40,
                   position: 'absolute',
                   left: 10,
                   bottom: 10
                 }}
-              />
+              >
+                <Image
+                  source={require('../assets/images/icon-photo-gallery-48-white.png')}
+                  style={{
+                    height: 40,
+                    width: 40,
+                  }}
+                />
+              </TouchableOpacity>
               <Button
                 onPress={() => this.takePicture()}
                 buttonStyle={{
@@ -116,4 +147,4 @@ class RoutesScreen extends React.Component {
   }
 }
 
-export default withNavigation(RoutesScreen);
+export default withNavigation(ImagePickerScreen);
