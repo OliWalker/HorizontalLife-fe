f291cce220c27a8be319538e05e178b1d11e15be
KarinaDavtyan <k.davtyan27@gmail.com>
2018-11-18 01:16:07 +0100

    Add routes' list layout

--- screens/RoutesScreen.js
+++ screens/RoutesScreen.js
@@ -5,7 +5,10 @@ import {
   Button,
   Platform,
   StatusBar,
-  Dimensions
+  Dimensions,
+  FlatList,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import ActionButton from 'react-native-action-button';
@@ -59,12 +62,77 @@ class RoutesScreen extends React.Component {
     this.props.navigation.navigate('ImagePicker');
   }
 
+  keyExtractor = item => item.route.id;
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          backgroundColor: '#CED0CE',
+        }}
+      />
+    );
+  };
 
   render() {
     return (
       <View>
         <StatusBar hidden />
-        <Text>Routes Screen</Text>
+        <FlatList
+          ItemSeparatorComponent={this.renderSeparator}
+          data={[
+            { route : { id: '1', name: 'Plastic tortilla', grade: '8C', new: true} },
+            { route: { id: '2', name: 'Bananas', grade: '8B', new: true } },
+            { route: { id: '3', name: 'Aloe', grade: '6A+', new: false } },
+            { route: { id: '4', name: 'Bezoya', grade: '5', new: false } },
+            { route: { id: '5', name: 'Mononoke', grade: '7C+', new: false } },
+          ]}
+          renderItem={({ item }) => 
+            <TouchableOpacity
+              style={{
+                flex: 0,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                height: 50,
+                backgroundColor: 'white',
+                alignItems: 'center',
+                paddingHorizontal: 10
+              }}
+            >
+              <View
+                style={{
+                  flex: 0,
+                  flexDirection: 'row'
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: '500'
+                  }}
+                >
+                  {item.route.name}
+                </Text>
+                {item.route.new && 
+                  <Image
+                    source={require('../assets/icons/new-50.png')}
+                    style={{
+                      height: 25,
+                      width: 25
+                    }}
+                  />}
+              </View>
+              <Text
+                style={{
+                  fontSize: 20
+                }}>
+                {item.route.grade}
+              </Text>
+            </TouchableOpacity>
+          }
+          keyExtractor={this.keyExtractor}
+        />      
         {Platform.OS == 'android' &&
         <ActionButton
           buttonColor={Colors.androidMain}
